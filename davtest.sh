def8cd3fdaddb129411b07ada8b6fd4d3fde740f
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2018-05-06 01:49:22 +0700

    Add files via upload

---
+++ davtest.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update && pkg upgrade
+pkg install wget
+pkg install perl
+pkg install zip
+echo
+echo "Davtest is downloading......"
+echo
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/davtest/davtest-1.0.zip
+echo
+echo "extracting file ........"
+echo
+unzip davtest-1.0.zip
+echo
+echo " hapus fie zip...... "
+rm davtest-1.0.zip
+echo
+echo  " ====================================== "
+echo    subscribe & like my channel youtube
+echo      regard,,boychongzen aka x-root
+echo  " ====================================== "
+echo
